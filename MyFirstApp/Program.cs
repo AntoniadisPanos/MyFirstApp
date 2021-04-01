d58e6b556966e85fbc6f51dc2fb8420f0207be4e
Panos Antoniadis <antoniadis.p1@gmail.com>
2021-04-01 19:24:29 +0300

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+
+        static void Line_vert(int x)
+
+        {
+            for (int i = 0; i < x; i++)
+            {
+
+            
+            
+                Console.WriteLine("*");
+            }
+        }
+        static void Line_horz(int x)
+
+        {
+            string answer = "";
+            for (int i = 0; i < x; i++)
+            {
+                answer += " *";
+            }
+            Console.WriteLine(answer);
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Line_vert(20);
+            Line_horz(20);
+            
+
+            //Console.Write("First Name: ");
+            //string First = Console.ReadLine();
+            //Console.Write("Last Name: ");
+            //string Last = Console.ReadLine();
+            //string FullName = First +  " " + Last;
+            //Console.WriteLine("FullName: " + FullName);
+            //Console.ReadKey();
+           
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
